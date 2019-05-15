822ce0a819e362e8ae7bbef779a14080691faa15
Djourn G.A. Gordon-Taylor Soyer <dsoyer12@gmail.com>
2019-05-14 22:44:59 -0400

    1st commit

---
+++ build/precache-manifest.48aa018b67c309f597bc9688610c2d21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85f3a821a9d70479b240a227ef61dc86",
+    "url": "/static/media/Website Prototype2.85f3a821.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d8f9ce021ff40dd39d48",
+    "url": "/static/js/main.d8f9ce02.chunk.js"
+  },
+  {
+    "revision": "8c5c6378f0fd2b11d1a5",
+    "url": "/static/js/2.8c5c6378.chunk.js"
+  },
+  {
+    "revision": "d8f9ce021ff40dd39d48",
+    "url": "/static/css/main.28589265.chunk.css"
+  },
+  {
+    "revision": "85f430f3f22318d6350d809a05e676b1",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.f8b3cc2fd3dfa2759c78455313719318.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e818aac5c7e7b43e33e325672333357c",
+    "url": "/static/media/azabu.e818aac5.jpg"
+  },
+  {
+    "revision": "b922a7bf8493ce50ea74",
+    "url": "/static/css/main.a779f12c.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "85f3a821a9d70479b240a227ef61dc86",
+    "url": "/static/media/Website Prototype2.85f3a821.png"
+  },
+  {
+    "revision": "bb85c3e5d5da74f28eb7",
+    "url": "/static/js/2.6d12686b.chunk.js"
+  },
+  {
+    "revision": "b922a7bf8493ce50ea74",
+    "url": "/static/js/main.d21bfb65.chunk.js"
+  },
+  {
+    "revision": "780e7bbecff99f774330f17046c1f520",
+    "url": "/static/media/ladybird.780e7bbe.jpeg"
+  },
+  {
+    "revision": "47f3537072cb9fdd5bd74c2ca509cea9",
+    "url": "/static/media/fishcheeks.47f35370.jpg"
+  },
+  {
+    "revision": "e0d650c37700978959c09db99c372d0c",
+    "url": "/static/media/hangawi.e0d650c3.jpg"
+  },
+  {
+    "revision": "bb85c3e5d5da74f28eb7",
+    "url": "/static/css/2.383ca5fc.chunk.css"
+  },
+  {
+    "revision": "9ec48ef4476e3a60a37691539b555dab",
+    "url": "/index.html"
+  }
+];
