deece9e87da0bf0462ace0824df24f8addb5d202
Djourn G.A. Gordon-Taylor Soyer <dsoyer12@gmail.com>
2019-05-16 02:53:04 -0400

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3151cf138146ad4b2ce18f796c0f2e95.js"
+  "./precache-manifest.9da5ca8bfc8fe32c8a74e5cfe5e388c6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
